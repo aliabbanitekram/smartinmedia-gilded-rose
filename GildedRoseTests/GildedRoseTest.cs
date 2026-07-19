c439415c741d702aae4191331bc87864c7354057
aliabbanitekram <aabbani@tekramdeliveries.com>
2026-07-19 15:37:22 +0200

    Refactor GildedRoseTest: Replace Foo test with comprehensive tests

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -1,17 +1,126 @@
-﻿using Xunit;
 using System.Collections.Generic;
 using GildedRoseKata;
+using Xunit;
 
 namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
     [Fact]
-    public void foo()
+    public void RegularItem_DecreasesSellInAndQuality()
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
+        Item item = UpdateSingleItem("+5 Dexterity Vest", sellIn: 10, quality: 20);
+
+        Assert.Equal(9, item.SellIn);
+        Assert.Equal(19, item.Quality);
+    }
+
+    [Fact]
+    public void RegularItem_DegradesTwiceAsFastAfterSellByDate()
+    {
+        Item item = UpdateSingleItem("+5 Dexterity Vest", sellIn: 0, quality: 20);
+
+        Assert.Equal(-1, item.SellIn);
+        Assert.Equal(18, item.Quality);
+    }
+
+    [Fact]
+    public void RegularItem_QualityNeverBecomesNegative()
+    {
+        Item item = UpdateSingleItem("+5 Dexterity Vest", sellIn: 0, quality: 0);
+
+        Assert.Equal(-1, item.SellIn);
+        Assert.Equal(0, item.Quality);
+    }
+
+    [Fact]
+    public void AgedBrie_IncreasesQualityAsItGetsOlder()
+    {
+        Item item = UpdateSingleItem("Aged Brie", sellIn: 2, quality: 0);
+
+        Assert.Equal(1, item.SellIn);
+        Assert.Equal(1, item.Quality);
+    }
+
+    [Fact]
+    public void AgedBrie_IncreasesQualityTwiceAsFastAfterSellByDate()
+    {
+        Item item = UpdateSingleItem("Aged Brie", sellIn: 0, quality: 10);
+
+        Assert.Equal(-1, item.SellIn);
+        Assert.Equal(12, item.Quality);
+    }
+
+    [Fact]
+    public void AgedBrie_QualityNeverIncreasesAboveFifty()
+    {
+        Item item = UpdateSingleItem("Aged Brie", sellIn: 2, quality: 50);
+
+        Assert.Equal(1, item.SellIn);
+        Assert.Equal(50, item.Quality);
+    }
+
+    [Fact]
+    public void Sulfuras_NeverChanges()
+    {
+        Item item = UpdateSingleItem("Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80);
+
+        Assert.Equal(0, item.SellIn);
+        Assert.Equal(80, item.Quality);
+    }
+
+    [Fact]
+    public void BackstagePass_IncreasesQualityByOneWhenMoreThanTenDaysRemain()
+    {
+        Item item = UpdateSingleItem("Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 20);
+
+        Assert.Equal(10, item.SellIn);
+        Assert.Equal(21, item.Quality);
+    }
+
+    [Fact]
+    public void BackstagePass_IncreasesQualityByTwoWhenTenDaysOrLessRemain()
+    {
+        Item item = UpdateSingleItem("Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 20);
+
+        Assert.Equal(9, item.SellIn);
+        Assert.Equal(22, item.Quality);
+    }
+
+    [Fact]
+    public void BackstagePass_IncreasesQualityByThreeWhenFiveDaysOrLessRemain()
+    {
+        Item item = UpdateSingleItem("Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 20);
+
+        Assert.Equal(4, item.SellIn);
+        Assert.Equal(23, item.Quality);
+    }
+
+    [Fact]
+    public void BackstagePass_QualityDropsToZeroAfterConcert()
+    {
+        Item item = UpdateSingleItem("Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 20);
+
+        Assert.Equal(-1, item.SellIn);
+        Assert.Equal(0, item.Quality);
+    }
+
+    [Fact]
+    public void BackstagePass_QualityNeverIncreasesAboveFifty()
+    {
+        Item item = UpdateSingleItem("Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49);
+
+        Assert.Equal(9, item.SellIn);
+        Assert.Equal(50, item.Quality);
+    }
+
+    private static Item UpdateSingleItem(string name, int sellIn, int quality)
+    {
+        IList<Item> items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
+        GildedRose app = new GildedRose(items);
+
         app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+
+        return items[0];
     }
 }
